35f4d8e49218384283b96b72de1ef1462d615dda
ghislaingenay <ghislain.gny@gmail.com>
2023-09-07 19:09:46 +0700

    mockup:finish animation for learn more

--- src/lib/starbucks/StarbucksMockUp.tsx
+++ src/lib/starbucks/StarbucksMockUp.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useEffect } from 'react';
 import Facebook from '@starbucks/assets/facebook.png';
 import Instagram from '@starbucks/assets/instagram.png';
 import X from '@starbucks/assets/x.png';
@@ -12,6 +12,11 @@ import StarbucksDrinkTwoSmall from '@starbucks/assets/thumb2.png';
 import StarbucksDrinkThreeSmall from '@starbucks/assets/thumb3.png';
 
 export default function StarbucksMockUp() {
+  // const getSVGElement = () =>
+  //   document.querySelector('a.learn-more svg') as HTMLElement;
+  // const getLearnMoreText = () =>
+  //   document.querySelector('span.learn-more') as HTMLSpanElement;
+
   return (
     <div id="starbucks" className="font-poppins">
       <nav className="starbucks-nav">
@@ -37,22 +42,31 @@ export default function StarbucksMockUp() {
           </li>
         </ul>
       </nav>
-      <main id="starbucks-main">
-        <section id="starbucks-banner" className="starbucks-hero">
-          <h2 className="starbucks-hero-title">
-            It's not just coffee.
-            <br />
-            It's <span className="bold">Starbucks</span>
-          </h2>
-          <p className="starbucks-hero-description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea tenetur
-            est voluptas assumenda hic, exercitationem iusto, illo eius ad is
-            delectus neque suscipit sapiente
-          </p>
-          <a href="#" className="learn-more">
-            Learn more
-          </a>
+      <main id="starbucks-banner">
+        <section className="starbucks-hero">
+          <div>
+            <h2 className="starbucks-hero-title">
+              It's not just Coffee.
+              <br />
+              It's <span className="bold">Starbucks</span>
+            </h2>
+            <p className="starbucks-hero-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              tenetur est voluptas assumenda hic, exercitationem iusto, illo
+              eius ad is delectus neque suscipit sapiente
+            </p>
+            <a href="#" className="learn-more">
+              <svg xmlns="http://www.w3.org/2000/svg">
+                <path id="learn-more-path" />
+              </svg>
+              <span className="learn-more">LEARN MORE</span>
+            </a>
+          </div>
+          <div>
+            <img src={StarbucksDrinkOne} alt="Starbucks Drink" />
+          </div>
         </section>
+        <section></section>
       </main>
     </div>
   );
