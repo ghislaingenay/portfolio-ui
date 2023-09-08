26829902df359c8aecb12b3f339359bacfffbe8e
ghislaingenay <ghislain.gny@gmail.com>
2023-09-08 17:02:19 +0700

    mockup:add basic button animation

--- src/lib/animations/Confetti.tsx
+++ src/lib/animations/Confetti.tsx
@@ -3,6 +3,7 @@ export const Confetti = () => {
     <main className="container grid" id="confetti">
       <button>
         <h1 className="font-montserrat">PARTY</h1>
+        <span className="font-montserrat"></span>
       </button>
     </main>
   );
