b51f0f362301490fb1532c8d763e3646b48f1f81
ghislaingenay <ghislain.gny@gmail.com>
2023-09-29 11:12:21 +0700

    finish hover light system

--- src/lib/animations/SpanCards.tsx
+++ src/lib/animations/SpanCards.tsx
@@ -1,28 +1,34 @@
 import { MouseEvent, useEffect } from 'react';
 
 function SpanCards() {
-  const handleMouseOver = (e: MouseEvent<HTMLDivElement>) => {
-    const { currentTarget: target } = e;
-    const rect = target && target.getBoundingClientRect();
-    // calculate the position of our mouse realtive to each card (x and y positions)
+  const handleMouseOver = (
+    e: MouseEvent<HTMLDivElement>,
+    cardElements: HTMLDivElement[]
+  ) => {
+    for (const card of cardElements) {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - (rect?.left || 0);
+      const y = e.clientY - (rect?.top || 0);
 
-    const x = e.clientX - (rect?.left || 0);
-    const y = e.clientY - (rect?.top || 0);
-
-    // set custom CSS properties to the card root to set a background position
-    target.style.setProperty('--mouse-x', `${x}px`);
-    target.style.setProperty('--mouse-y', `${y}px`);
+      card.style.setProperty('--mouse-x', `${x}px`);
+      card.style.setProperty('--mouse-y', `${y}px`);
+    }
   };
 
   useEffect(() => {
     const cardElements = document.querySelectorAll(
       '#span-cards .card'
     ) as unknown as HTMLDivElement[];
-    for (const card of cardElements) {
-      card.addEventListener('mousemove', (event) =>
-        handleMouseOver(event as unknown as MouseEvent<HTMLDivElement>)
-      );
-    }
+    const cardsElement = document.querySelector(
+      '#span-cards .cards'
+    ) as unknown as HTMLDivElement;
+
+    cardsElement.addEventListener('mousemove', (event) =>
+      handleMouseOver(
+        event as unknown as MouseEvent<HTMLDivElement>,
+        cardElements
+      )
+    );
   }, []);
   return (
     <main id="span-cards">
