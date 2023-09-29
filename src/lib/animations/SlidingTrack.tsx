423dcf7bf1adb4370739fc365dbde3eab2c337d0
ghislaingenay <ghislain.gny@gmail.com>
2023-09-29 17:58:15 +0700

    sliding:pan the padding with mouve over

--- src/lib/animations/SlidingTrack.tsx
+++ src/lib/animations/SlidingTrack.tsx
@@ -6,50 +6,84 @@ import SlidingImage5 from '@animations/assets/sliding-track/image-5.jpg';
 import SlidingImage6 from '@animations/assets/sliding-track/image-6.jpg';
 import SlidingImage7 from '@animations/assets/sliding-track/image-7.jpg';
 import SlidingImage8 from '@animations/assets/sliding-track/image-8.jpg';
+import { useEffect } from 'react';
 
 function SlidingTrack() {
+  useEffect(() => {
+    const maxDelta = window.innerWidth / 2; // all the animation of the 8 pictures are done at 50% of the window size
+    const track = document.getElementById('image-track') as HTMLElement;
+    window.addEventListener('mousedown', (e) => {
+      track.dataset.mouseDownAt = e.clientX.toString();
+    });
+
+    window.addEventListener('mousemove', (e) => {
+      if (track.dataset.mouseDownAt === '0') return;
+      if (track.dataset.mouseDownAt) {
+        const distance = Number(track.dataset.mouseDownAt) - e.clientX;
+        const percentage = -(distance / maxDelta) * 100;
+        const nextPercentage =
+          Number(track.dataset.prevPercentage) + percentage;
+        track.dataset.percentage = nextPercentage.toString();
+        track.style.transform = `translate(${nextPercentage}%, -50%)`;
+      }
+    });
+
+    window.addEventListener('mouseup', (e) => {
+      track.dataset.mouseDownAt = '0';
+      track.dataset.prevPercentage = track.dataset.percentage;
+      // track.style.transform = `translate(0%, -50%)`;
+    });
+  }, []);
   return (
     <main id="sliding-track">
-      <div id="image-track">
+      <div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
         <img
           src={SlidingImage1}
           alt="Unsplash images"
           className="sliding-img"
+          draggable={false}
         />
         <img
           src={SlidingImage2}
           alt="Unsplash images"
           className="sliding-img"
+          draggable={false}
         />
         <img
           src={SlidingImage3}
           alt="Unsplash images"
           className="sliding-img"
+          draggable={false}
         />
         <img
           src={SlidingImage4}
           alt="Unsplash images"
           className="sliding-img"
+          draggable={false}
         />
         <img
           src={SlidingImage5}
           alt="Unsplash images"
           className="sliding-img"
+          draggable={false}
         />
         <img
           src={SlidingImage6}
           alt="Unsplash images"
           className="sliding-img"
+          draggable={false}
         />
         <img
           src={SlidingImage7}
           alt="Unsplash images"
           className="sliding-img"
+          draggable={false}
         />
         <img
           src={SlidingImage8}
           alt="Unsplash images"
           className="sliding-img"
+          draggable={false}
         />
       </div>
     </main>
