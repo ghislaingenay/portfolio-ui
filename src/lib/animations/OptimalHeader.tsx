3fc04cb45952a4c34f0a6668e23e1d80d81fdc25
ghislaingenay <ghislain.gny@gmail.com>
2023-09-29 11:24:58 +0700

    Add hidden header system

--- src/lib/animations/OptimalHeader.tsx
+++ src/lib/animations/OptimalHeader.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function OptimalHeader() {
-  return <div>OptimalHeader</div>;
+  return (
+    <main id="opti-main">
+      <div className="opti-side" id="opti-left-side">
+        <h2 className="title">Today is going to be awesome</h2>
+      </div>
+      <div className="opti-side" id="opti-right-side">
+        <h2 className="title">Today is going to be wonderful</h2>
+      </div>
+    </main>
+  );
 }
 
 export { OptimalHeader };
